deb1a7626a77aaf2bdb5101aa203690d63c62016
MatinHosz <matin.hosseinzadeh06@gmail.com>
2025-03-24 21:26:15 +0330

    refactor the structure of the project

--- FizzBuzz/src/Main.java
+++ FizzBuzz/src/Main.java
@@ -1,35 +1,45 @@
+import java.util.Map;
+import java.util.HashMap;
+
 public class Main {
-    static String output = "";
-
+    static Map<Integer, String> baseNumberToCueWord = new HashMap<>();
     public static void main(String[] args) {
+        Map<Integer, String> baseNumberToCue = new HashMap<Integer,String>();
+        baseNumberToCue.put(3, "Fizz");
+        baseNumberToCue.put(5, "Buzz");
 
-        final int baseNumber_1 = 3;
-        final int baseNumber_2 = 5;
-        final int baseNumber_3 = 7;
-
+        FizzBuzzProcessor fizzBuzzProcessor = new FizzBuzzProcessor(baseNumberToCue);
         for (int i = 1; i <= 100; i++) {
-            appendCueIfMultiple(baseNumber_1, i, "Fizz");
-            appendCueIfMultiple(baseNumber_2, i, "Buzz");
-            appendCueIfMultiple(baseNumber_3, i, "Choz");
-
-            printOutput(i);
+            String result = fizzBuzzProcessor.appendCueIfMultiple(i);
+            fizzBuzzProcessor.printResult(i, result);
         }
+    }
+}
 
+
+class  FizzBuzzProcessor {
+    private Map<Integer, String> baseNumberToCueWord = new HashMap<>();
+
+    public FizzBuzzProcessor(Map<Integer, String> baseNumberToCueWord) {
+        this.baseNumberToCueWord = baseNumberToCueWord;
     }
 
-    private static void appendCueIfMultiple(int baseNumber, int number, String cueWord) {
+    public String appendCueIfMultiple(int number) {
+        StringBuilder output = new StringBuilder();
+        baseNumberToCueWord.forEach((baseNumber, cueWord) -> {
         if (number % baseNumber == 0) {
-            output += cueWord;
+            output.append(cueWord);
         }
+        });
+        return output.toString();
     }
 
-    private static void printOutput(int number) {
-        if (output.isEmpty()) {
-            output = Integer.toString(number);
-            System.out.println(output);
+    public void printResult(int number, String result) {
+        if (result.isEmpty()) {
+            result = Integer.toString(number);
+            System.out.println(result);
         } else {
-            System.out.println(output);
+            System.out.println(result);
         }
-        output = "";
     }
 }
