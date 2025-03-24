989e03d320b44bd78f6a19cd3428f363f1d299b1
MatinHosz <matin.hosseinzadeh06@gmail.com>
2025-03-24 15:34:43 +0330

    Create basenumbers and function for check multiples of numbers

--- FizzBuzz/src/Main.java
+++ FizzBuzz/src/Main.java
@@ -1,4 +1,19 @@
 public class Main {
+    static String output = "";
+
     public static void main(String[] args) {
+
+        final int baseNumber_1 = 3;
+        final int baseNumber_2 = 5;
+        final int baseNumber_3 = 7;
+
+        
+
+    }
+
+    private static void appendCueIfMultiple(int baseNumber, int number, String cueWord) {
+        if (number % baseNumber == 0) {
+            output += cueWord;
+        }
     }
 }
